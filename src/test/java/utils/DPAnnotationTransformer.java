b3e66e9eb328aa61deec01cb53eefb2f1e934c34
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-11-20 18:46:51 +0200

    initial commit

--- src/test/java/utils/DPAnnotationTransformer.java
+++ src/test/java/utils/DPAnnotationTransformer.java
@@ -10,6 +10,7 @@ public class DPAnnotationTransformer implements IAnnotationTransformer {
     @Override
     public void transform(ITestAnnotation iTestAnnotation, Class aClass, Constructor constructor, Method method) {
         if(method.getName().toLowerCase().equals("parametrizedtest")) {
+            System.out.println("Will use method: " + method.getName());
             if(!System.getProperty("dp").toLowerCase().equals("empty")) {
                 iTestAnnotation.setDataProviderClass(DPSource.class);
                 iTestAnnotation.setDataProvider(System.getProperty("dp"));
